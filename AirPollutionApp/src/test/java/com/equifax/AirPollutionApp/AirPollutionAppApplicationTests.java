db285c1200497fa3103ec1b20642cfa31131ba9c
venka <venka@DESKTOP-S7J9A6M>
2022-04-24 14:22:19 +0530

    InitialCommit

---
+++ AirPollutionApp/src/test/java/com/equifax/AirPollutionApp/AirPollutionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equifax.AirPollutionApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirPollutionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
